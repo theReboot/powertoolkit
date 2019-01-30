eac8deacac2486faefafa9bbc2383c0f9b874413
bayo <bayokrapht@gmail.com>
2019-01-30 14:49:26 +0100

    removed contact page and replaced with snippet

--- powerkit/contact/context_processors.py
+++ powerkit/contact/context_processors.py
@@ -1,10 +1,11 @@
-from contact.models import ContactPage
+from contact.models import Contact
 
 
 def contact_info(request):
-    contacts = ContactPage.objects.all()
+    contacts = Contact.objects.all()
     if contacts:
         return {
+            'intro': contacts[0].intro,
             'email': contacts[0].email,
             'phone': contacts[0].phone,
             'address': contacts[0].address
--- powerkit/contact/models.py
+++ powerkit/contact/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
-from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.models import register_snippet
 
 
-class ContactPage(Page):
+@register_snippet
+class Contact(models.Model):
     intro = models.TextField(blank=True)
     email = models.CharField(max_length=100, blank=True)
     phone = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=200, blank=True)
 
-    content_panels = Page.content_panels + [
+    panels = [
         FieldPanel('intro', classname='full'),
         FieldPanel('email'),
         FieldPanel('phone'),

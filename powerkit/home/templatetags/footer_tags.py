91a3990d77012edd331df4009b4e8e841d3c295c
bayo <bayokrapht@gmail.com>
2019-01-30 14:49:44 +0100

    modified footer

--- powerkit/home/templatetags/footer_tags.py
+++ powerkit/home/templatetags/footer_tags.py
@@ -1,16 +1,34 @@
 from django import template
 from home.models import FooterText
 from issue.models import IssuePage
+from contact.models import Contact
 
 register = template.Library()
 
 
 @register.inclusion_tag('home/footer.html')
 def footer():
-    _footers = FooterText.objects.all()
-    if _footers:
-        footer_text = _footers[0].text
+    _contacts = Contact.objects.all()
+    if _contacts:
+        _contact = _contacts[0]
+    else:
+        _contact = {
+            'intro': '',
+            'phone': '',
+            'email': '',
+            'address': ''
+        }
+        
+        # _footers = _contact.intro
+        # _email = _contact.email
+        # _phone = _contact.phone
+        # _address = _contact.address
+        return
+
+    # _footers = FooterText.objects.all()
+    # if _footers:
+    #     footer_text = _footers[0].text
     return {
-        'footer_text': footer_text,
+        'contact': _contact,
         'issues': IssuePage.objects.all()
     }

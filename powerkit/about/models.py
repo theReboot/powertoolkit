443bb555cc541a49c44a3c14c645ffa79aee914c
bayo <bayokrapht@gmail.com>
2019-02-28 21:50:05 +0100

    snippet for metadata

--- powerkit/about/models.py
+++ powerkit/about/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.snippets.models import register_snippet
 
 
 class AboutPage(Page):
@@ -10,3 +11,20 @@ class AboutPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full'),
     ]
+
+
+@register_snippet
+class Metadata(models.Model):
+    description = models.CharField(max_length=250)
+    keywords = models.TextField(blank=True, null=True)
+
+    panels = [
+        FieldPanel('description', classname='full'),
+        FieldPanel('keywords', classname='full'),
+    ]
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name_plural = 'Metadata'

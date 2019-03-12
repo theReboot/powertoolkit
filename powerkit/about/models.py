6146088e4ccb0d7fb1142a5091c73ea8509a7746
bayo <bayokrapht@gmail.com>
2019-03-12 01:01:25 +0100

    added partner snippets for about page

--- powerkit/about/models.py
+++ powerkit/about/models.py
@@ -3,10 +3,16 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.snippets.models import register_snippet
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class AboutPage(Page):
     intro = models.TextField(blank=True)
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['partners'] = Partner.objects.all()
+        return context
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full'),
@@ -28,3 +34,23 @@ class Metadata(models.Model):
 
     class Meta:
         verbose_name_plural = 'Metadata'
+
+
+@register_snippet
+class Partner(models.Model):
+    name = models.CharField(max_length=250)
+    logo = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    def __str__(self):
+        return self.name
+
+    panels = [
+        FieldPanel('name', classname='full'),
+        ImageChooserPanel('logo'),
+    ]
